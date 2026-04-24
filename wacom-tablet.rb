4530c6ec3b6140a47aef0af08b841e8fd99f16da
asd <asd@asds-MacBook-Pro.local>
2026-04-24 14:10:41 +0300

    added 6.4.10-3 version of wacom driver

---
+++ wacom-tablet.rb
@@ -0,0 +1,44 @@
+cask "wacom-tablet" do
+  version "6.4.10-3"
+  sha256 "9f740930f3ccefb453d80a4eb7d78acba5f6373f9dd4310bf859e59df2bf5106"
+
+  url "https://cdn.wacom.com/u/productsupport/drivers/mac/professional/WacomTablet_#{version}.dmg"
+  name "Wacom Tablet"
+  desc "Resources for Wacom tablets"
+  homepage "https://www.wacom.com/en-us/support/product-support/drivers"
+
+  livecheck do
+    url :homepage
+    regex(%r{/WacomTablet[._-]?v?(\d+(?:\.\d+)+(?:[_-]\d+[a-z]?)?)\.dmg}i)
+  end
+
+  depends_on macos: ">= :monterey"
+
+  pkg "Install Wacom Tablet.pkg"
+
+  uninstall launchctl: [
+              "com.wacom.DataStoreMgr",
+              "com.wacom.IOManager",
+              "com.wacom.TabletDriver*",
+              "com.wacom.UpdateHelper",
+              "com.wacom.UpgradeHelper*",
+              "com.wacom.WacomCenter*",
+              "com.wacom.WacomExperienceProgram*",
+              "com.wacom.wacomtablet",
+              "Wacom_IOManager",
+            ],
+            quit:      [
+              "com.wacom.wacomtablet",
+              "com.wacom.WacomTouchDriver",
+            ],
+            pkgutil:   "com.wacom.TabletInstaller"
+
+  zap trash: [
+    "~/Library/Application Scripts/*.com.wacom.WacomTabletDriver",
+    "~/Library/Application Scripts/com.wacom.*",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.wacom.wacomexperienceprogram.sfl*",
+    "~/Library/Containers/com.wacom.*",
+    "~/Library/Group Containers/*.com.wacom.WacomTabletDriver",
+    "~/Library/Group Containers/com.wacom.TabletDriver",
+  ]
+end
