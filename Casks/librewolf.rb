acdeda2074f7654712135f65b5fc7b2367858ea0
asd <macintoshkrug@gmail.com>
2026-04-24 14:32:46 +0300

    added csp and librewolf

---
+++ Casks/librewolf.rb
@@ -0,0 +1,46 @@
+cask "librewolf" do
+  arch arm: "arm64"
+
+  version "150.0,1"
+  sha256 arm:   "f5a282b4a06ccc6ea9f94ba91f6452360c4f668b3e90ecf2f8e7018713846162"
+
+  url "https://codeberg.org/api/packages/librewolf/generic/librewolf/#{version.tr(",", "-")}/librewolf-#{version.tr(",", "-")}-macos-#{arch}-package.dmg",
+      verified: "codeberg.org/api/packages/librewolf/generic/librewolf/"
+  name "LibreWolf"
+  desc "Web browser"
+  homepage "https://librewolf.net/"
+
+  # There can be a notable gap between when a version is tagged and a
+  # corresponding release is created, so we check the "latest" release instead
+  # of the Git tags.
+  livecheck do
+    url "https://codeberg.org/api/v1/repos/librewolf/bsys6/releases/latest"
+    regex(/^v?(\d+(?:[.-]\d+)+)$/i)
+    strategy :json do |json, regex|
+      json["tag_name"]&.[](regex, 1)&.tr("-", ",")
+    end
+  end
+
+  depends_on :macos
+
+  app "LibreWolf.app"
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/librewolf.wrapper.sh"
+  binary shimscript, target: "librewolf"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/sh
+      exec '#{appdir}/LibreWolf.app/Contents/MacOS/librewolf' "$@"
+    EOS
+  end
+
+  zap trash: [
+    "~/.librewolf",
+    "~/Library/Application Support/LibreWolf",
+    "~/Library/Caches/LibreWolf Community",
+    "~/Library/Caches/LibreWolf",
+    "~/Library/Preferences/io.gitlab.librewolf-community.librewolf.plist",
+    "~/Library/Saved Application State/io.gitlab.librewolf-community.librewolf.savedState",
+  ]
+end
