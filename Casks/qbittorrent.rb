6959837f7f822bc6e343adb06725936cc10dd813
asd <macintoshkrug@gmail.com>
2026-04-24 17:44:18 +0300

    rearrange

--- Casks/qbittorrent.rb
+++ Casks/qbittorrent.rb
@@ -2,18 +2,17 @@ cask "qbittorrent" do
   version "5.0.5"
   sha256 "7285e98b5ef4b9a227247dcd933f1a556bfc1ff10901fc7aedb0c4b7ad082814"
 
-  livecheck do
-    url "https://sourceforge.net/projects/qbittorrent/rss?path=/qbittorrent-mac"
-    regex(%r{url=.*?/qbittorrent[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
-  end
-
-
   url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-mac/qbittorrent-#{version}/qbittorrent-#{version}.dmg",
-      verified: "downloads.sourceforge.net/qbittorrent/qbittorrent-mac/"
+  verified: "downloads.sourceforge.net/qbittorrent/qbittorrent-mac/"
   name "qBittorrent"
   desc "Peer to peer Bitorrent client"
   homepage "https://www.qbittorrent.org/"
 
+  livecheck do
+    url "https://sourceforge.net/projects/qbittorrent/rss?path=/qbittorrent-mac"
+    regex(%r{url=.*?/qbittorrent[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+  end
+  
   depends_on :macos
 
   # Renamed for consistency: app name is different in the Finder and in a shell.
